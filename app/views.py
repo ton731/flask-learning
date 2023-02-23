b50e5a8e04630a3be2fe956eda165d6c6c492a7c
Yuan-Tung Chou <ton731@gmail.com>
2023-02-23 19:17:34 +0800

    add dynamic url, json, fetch API

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, jsonify, make_response
 
 
 @app.route("/")
@@ -45,3 +45,83 @@ def sign_up():
         return redirect(request.url)
 
     return render_template("public/sign_up.html")
+
+
+
+
+# fake database using dictionary
+users = {
+    'mitsuhiko': {
+        "name": "Armin Ronacher",
+        "bio": "Creator of the Flask framework",
+        "twitter_handle": "@mitsuhuijo"
+    },
+    "gvanrossum": {
+        "name": "Guido Van Rossum",
+        "bio": "Creator of the Python programming language",
+        "twitter_handle": "@gvanrossum"
+    },
+    "elonmusk": {
+        "name": "Elon Musk",
+        "bio": "technology entrepreneuer, investor, and engineer",
+        "twitter_handle": "@elonmusk"
+    }
+}
+
+
+# dynamic url with one vairable
+@app.route("/profile/<username>")
+def profile(username):
+
+    user = None
+    if username in users:
+        user = users[username]
+
+    return render_template("public/profile.html", username=username, user=user)
+
+
+# dynamic url with multiple variables
+@app.route("/multiple/<a>/<b>/<c>")
+def multiple(a, b, c):
+    return f"a: {a}, b: {b}, c: {c}"
+
+
+# json with flask
+@app.route("/json", methods=["POST"])
+def json():
+
+    if request.is_json:
+        req = request.get_json()
+        response = {
+            "message": "JSON reveived",
+            "name": req.get("name")
+        }
+        res = make_response(jsonify(response), 200)
+
+        return res
+    
+    else:
+        res = make_response(jsonify({"message": "No JSON reveived"}), 400)
+        return res
+    
+
+# flask and fetch API
+@app.route("/guestbook")
+def guestbook():
+    return render_template("public/guestbook.html")
+
+
+@app.route("/guestbook/create-entry", methods=["POST"])
+def create_entry():
+
+    req = request.get_json()
+    print(req)
+
+    res = make_response(jsonify(req), 200)
+
+    return res
+
+
+
+
+
